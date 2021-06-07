102e89cf4660335f7f4c10cba990ce64f0a0e5e0
reidtc82 <reidtc82@gmail.com>
2021-06-06 18:51:53 -0700

    menu work

--- src/bank_system.py
+++ src/bank_system.py
@@ -1,61 +1,141 @@
 class Driver:
     __prompt = f"Enter menu selection: "
-    __menus = {
-        "main_menu": [
-            {"text": "Account Manager", "value": 0},
-            {"text": "GL Manager", "value": 1},
-            {"text": "Exit", "value": -1},
-        ],
-        "account_menu": [{"text": "Create New", "value": 2},{"text":"Manage Existing", "value": 3},{"text":"Return...","value": 4}],
-        "gl_menu": [],
-        "loan_menu": [],
-    }
+    __menus = {}
+
     account_list = []
     people_list = []
     loan_list = []
     applications_list = []
+    gl_list = []
 
     def __init__(self):
-        pass
+        self.__menus = {
+            "Main Menu": {
+                "Account Manager": self.account_menu,
+                "GL Manager": self.gl_menu,
+                "Loan Manager": self.loan_menu,
+                "Customer Manager": self.customer_menu,
+                "Exit": self.end_program,
+            },
+            "Account Menu": {
+                "Create New": self.create_account,
+                "Manage Existing": self.manage_account,
+                "Return": self.main_menu,
+            },
+            "GL Menu": {
+                "Create New": self.create_gl,
+                "Manage Existing": self.manage_gl,
+                "Return": self.main_menu,
+            },
+            "Loan Menu": {
+                "New Loan Application": self.create_application,
+                "Work Application": self.work_application,
+                "Work Existing Loan": self.work_loan,
+                "Return": self.main_menu,
+            },
+            "Customer Menu": {
+                "New Customer": self.create_customer,
+                "Manage Existing": self.manage_customer,
+                "Return": self.main_menu,
+            },
+        }
 
-    def main_menu(self) -> int:
-        print(f"Main Menu:")
-        print(f"(1) {self.__menus['main_menu'][0]['text']}")
-        print(f"(2) {self.__menus['main_menu'][1]['text']}")
-        print(f"(3) {self.__menus['main_menu'][2]['text']}")
-        return int(input(self.__prompt))
+    def menu_handler(self, this_menu):
+        print(f"\n{this_menu}")
+        menu_items = list(self.__menus[this_menu].keys())
+        for i, v in enumerate(menu_items):
+            print(f"{i} {v}")
+        selection = int(input("Enter selection... "))
+        self.__menus[this_menu][menu_items[selection]]()
 
-    def account_menu(self) -> int:
-        print(f"Account Manager")
-        print(f"(1) Create new account")
-        print(f"(2) Existing account")
-        print(f"(3) Return")
-        return int(input(self.__prompt))
+    def main_menu(self):
+        self.menu_handler("Main Menu")
+
+    def account_menu(self):
+        self.menu_handler("Account Menu")
+
+    def gl_menu(self):
+        self.menu_handler("GL Menu")
+
+    def loan_menu(self):
+        self.menu_handler("Loan Menu")
+
+    def customer_menu(self):
+        self.menu_handler("Customer Menu")
+
+    def create_account(self):
+        print(f"\nAccount Creation")
+        if not self.people_list:
+            print(f"No Customers exist. Please create a customer before an Account...")
+            self.menu_handler("Customer Menu")
+        else:
+            pass
+
+    def manage_account(self):
+        print(f"\nManaging an Account")
+        if not self.account_list:
+            print(f"No Accounts exist. Please create an Account...")
+            self.menu_handler("Account Menu")
+        else:
+            pass
+
+    def create_gl(self):
+        print(f"\nGL Creation")
+
+    def manage_gl(self):
+        print(f"\nManaging a GL")
+        if not self.gl_list:
+            print(f"No GLs exist. Please create a GL...")
+            self.menu_handler("GL Menu")
+        else:
+            pass
+
+    def create_application(self):
+        print(f"\nApplication Creation")
+        if not self.people_list:
+            print(f"No Customers exist. PLease create a Customer first...")
+            self.menu_handler("Customer Menu")
+        elif not self.account_list:
+            print(f"No Accounts exist. Please create an Account first...")
+            self.menu_handler("Account Menu")
+        else:
+            pass
+
+    def work_application(self):
+        print(f"\nWorking an Application")
+        if not self.applications_list:
+            print(f"No Applications exist. Please create an Application...")
+            self.menu_handler("Loan Menu")
+        else:
+            pass
+
+    def work_loan(self):
+        print(f"\nWorking a loan")
+        if not self.loan_list:
+            print(f"No Loans exist. Please book a Loan from an Application...")
+            self.menu_handler("Loan Menu")
+        else:
+            pass
+
+    def create_customer(self):
+        print(f"\nCustomer Creation")
+
+    def manage_customer(self):
+        print(f"\nManaging a customer")
+        if not self.people_list:
+            print(f"No Customers exist. PLease create a Customer...")
+            self.menu_handler("Customer Menu")
+        else:
+            pass
 
     def end_program(self):
-        # Do all the clean up and saving of stuff
         quit()
 
 
 if __name__ == "__main__":
     print(f"Welcome to BankSys")
     driver = Driver()
-    selection = driver.main_menu()
-    while selection >= 0:
-        if selection == -1:
-            break
-        elif selection == 0:
-            selection = driver.account_menu()
-        elif selection == 1:
-            selection = driver.gl_menu()
-        elif selection == 2:
-            selection = driver.create_account()
-        elif selection == 3:
-            selection = driver.manage_existing()
-        
-        else:
-            selection = driver.main_menu()
 
-        
+    driver.menu_handler("Main Menu")
 
     driver.end_program()
